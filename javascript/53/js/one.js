a1080e841f2672f7012ba017afa1bfb288ae8cf0
shirashiffman <shirashiffman@gmail.com>
2020-07-02 20:58:53 -0400

    Homework 53

---
+++ javascript/53/js/one.js
@@ -0,0 +1,3 @@
+"use strict";
+var name = "Shira";
+var email = "shirashiffman@gmail.com";
---
+++ javascript/53/js/two.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log(name, " ", email);
+
+var age = window.prompt("What is your age?");
+console.log(age);
