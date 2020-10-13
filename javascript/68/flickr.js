71da5e519da358a77a577de9ff0e0dbff5fd326e
shirashiffman <shirashiffman@gmail.com>
2020-10-13 19:51:07 -0400

    Homework 68

--- javascript/68/flickr.js
+++ javascript/68/flickr.js
@@ -3,9 +3,12 @@
   "use strict";
 
   const picContainerElem = $("#picContainer");
+  const carousel = $("#carousel");
+  //carousel.hide();
   picContainerElem.hide();
 
   $("#searchButton").click(function () {
+    picContainerElem.empty();
     let searchVal = $("#searchBox").val();
     picContainerElem.show();
     $.getJSON(
@@ -15,10 +18,35 @@
 
         let picArray = data.items;
         picArray.forEach((imgObject) => {
-          $(`<div ><img src=${imgObject.media.m}></div>`).appendTo(
-            picContainerElem
-          );
-          //console.log(imgObject);
+          $(`<div ><img src=${imgObject.media.m}></div>`)
+            .appendTo(picContainerElem)
+            .append($(`<span class="pictureTitle">${imgObject.title}</span>`));
+          console.log(imgObject);
+        });
+        const rightArrow = $("#rightArrow");
+        const leftArrow = $("#leftArrow");
+        let sourceIndex = 0;
+        console.log("clicked");
+        document.getElementById(
+          "carousel"
+        ).style.backgroundImage = `url(${picArray[sourceIndex].media.m})`;
+
+        rightArrow.click(function () {
+          if (sourceIndex === picArray.length - 1) {
+            sourceIndex = -1;
+          }
+
+          document.getElementById("carousel").style.backgroundImage = `url(${
+            picArray[++sourceIndex].media.m
+          })`;
+        });
+        leftArrow.click(function () {
+          if (sourceIndex === 0) {
+            sourceIndex = picArray.length;
+          }
+          document.getElementById("carousel").style.backgroundImage = `url(${
+            picArray[--sourceIndex].media.m
+          })`;
         });
       }
     );
