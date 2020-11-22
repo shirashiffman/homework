1d7f8ee99e08ffe6ca5cbb436c4c103f5071a3a0
shirashiffman <shirashiffman@gmail.com>
2020-11-22 11:05:43 -0500

    79

---
+++ javascript/79/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+//const json5 = require('json5');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+
+      // {
+      //   test: /\.json5$/i,
+      //   type: 'json',
+      //   parser: {
+      //     parse: json5.parse,
+      //   },
+      // },
+    ]
+  }
+};
